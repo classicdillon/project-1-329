7a885c5038b5ad6ea422a77cc0ef21beba78395d
Dillon McDonald <dillonmcdonald808@gmail.com>
2021-04-11 13:35:34 -0700

    Card componenent added

--- js/webcomponents.js
+++ js/webcomponents.js
@@ -4,11 +4,35 @@
 // Web Components Classes Here
 //////////////////////////////////
 
-class TemplateExample extends HTMLElement {
-    constructor(){
+// class TemplateExample extends HTMLElement {
+//     constructor(){
+//         super()
+
+//         this.innerHTML = `<h1>Hello World</h1>`
+//     }
+// }
+
+class MyCard extends HTMLElement {
+    constructor() {
         super()
 
-        this.innerHTML = `<h1>Hello World</h1>`
+       const name = this.getAttribute("name")
+       const git = this.getAttribute("git")
+       const live = this.getAttribute("live")
+       const img = this.getAttribute("img")
+
+
+       this.innerHTML = `
+       <sl-card>
+       <h1>${name}</h1>
+       <img src=${img} alt=${name} slot="image">
+       <sl-button-group>
+  <a href=${git}><sl-button>GIT</sl-button></a>
+  <a href=${live}><sl-button>LIVE</sl-button></a>
+</sl-button-group>
+       
+       
+       </sl-card>`
     }
 }
 
@@ -18,4 +42,5 @@ class TemplateExample extends HTMLElement {
 // Register the HTML Tag here
 /////////////////////////////////
 
-customElements.define("template-example", TemplateExample)
+// customElements.define("template-example", TemplateExample)
+customElements.define("my-card", MyCard)
